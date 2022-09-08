a0d577c1d65347e2ba6f5ab0ac256609489b2f01
Leon <leonmoreira199@gmail.com>
2022-09-08 18:36:11 -0300

    Adicionando a interatividade

---
+++ main.js
@@ -0,0 +1,117 @@
+
+const bill = document.querySelector('.inputbill')
+const custom = document.querySelector('.custom')
+const tipBtns = document.querySelectorAll('.btn')
+const numberpeople = document.querySelector('.peopleinput')
+const tiptotal = document.querySelector('.tiptotal')
+const person = document.querySelector('.tipperson')
+const btnrest = document.querySelector('.buttonreset')
+const errorMsg = document.querySelector('.error-msg')
+const Totalresults = document.querySelectorAll('.tiptotal')
+const Divideresults = document.querySelectorAll('.tipperson')
+const restbtn = document.querySelector('.buttonreset')
+
+bill.addEventListener('input', setBillValue);
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', handleCLick)
+})
+custom.addEventListener('input', setCustomValue)
+numberpeople.addEventListener('input', setPersonValue) 
+
+let billValue = 0.0
+let tipValue = 0.15
+let personvalue = 1
+
+function validadeFloat(s){
+    var rgx = /^[0-9]*\.?[0-9]*$/;
+    return s.match(rgx)
+}
+
+function validadeInt(s){
+    var rgx = /^[0-9]*$/
+    return s.match(rgx)
+}
+
+function setBillValue() {
+    if(bill.value.includes(',')){
+        bill.value = bill.value.replace(',','.')
+    }
+    // Muda de , para .
+
+    if(!validadeFloat(bill.value)){
+        bill.value = bill.value.substring(0, bill.value.length-1)
+    }
+
+    // Não permite que digite caracteres diferentes indevidos
+
+
+    billValue = parseFloat(bill.value);
+
+    calculateTip()
+
+}
+
+function handleCLick(event){
+    tipBtns.forEach(btn => {
+        //Apagar estado ativo
+        btn.classList.remove('buttonactive')
+
+        // Adicionar estado ativo
+        if(event.target.innerHTML == btn.innerHTML){
+            btn.classList.add('buttonactive')
+            tipValue = btn.value
+        }
+    })
+}
+
+//Clear custom tip
+custom.value = ''
+
+calculateTip()
+
+function setCustomValue() {
+    if(!validadeInt(custom.value)){
+        custom.value = custom.value.substring(0, custom.value.length-1)
+    } // Apenas digitar números
+
+    tipValue = parseFloat(custom.value/100);
+
+    // Remove status ativo dos botões 
+    tipBtns.forEach(btn => {
+        btn.classList.remove('buttonactive')
+    })
+
+    if(custom.value !== ''){
+        calculateTip()
+    }
+    
+}
+
+function setPersonValue(){
+    if(!validadeInt(numberpeople.value)){
+        numberpeople.value = numberpeople.value.substring(0, numberpeople.value.length-1)
+    }
+
+    personvalue = parseFloat(numberpeople.value)
+
+    if(personvalue <= 0){
+        errorMsg.classList.add('show-error-msg')
+        setTimeout(function(){
+            errorMsg.classList.remove('show-error-msg')
+        },3000);
+
+        calculateTip()
+    }
+}
+
+function calculateTip(){
+    if (personvalue >= 1){
+        let tipAmount = (billValue * tipValue) / personvalue
+        let total = billValue * (tipValue + 1) / personvalue
+        
+        Totalresults.innerHTML = '$' + tipAmount.toFixed(2)
+        Divideresults.innerHTML = '$' + total.toFixed(2)
+
+        console.log(tipAmount)
+    }
+}
